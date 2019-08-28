a2806b190814e992d605823149ed966622d398e6
Dmitry16 <europartner@inbox.ru>
2019-08-28 19:40:53 +0300

    inference

--- tmp.ts
+++ tmp.ts
@@ -6,10 +6,10 @@ const l = last([1, 2, 3]);
 
 const l2 = last(['a', 'b', 'c']);
 
-const makeArr = <T, Y>(x: T, y: Y) => {
-  return [x];
+const makeArr = <X, Y>(x: X, y: Y): [X, Y] => {
+  return [x, y];
 }
 
 const m = makeArr(5, 6);
-
 const m2 = makeArr('a', 'b');
+const m3 = makeArr('a', 7);
