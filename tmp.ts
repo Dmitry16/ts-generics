069c9f1a9e9d1c59aa10103507d54a45f42733d6
Dmitry16 <europartner@inbox.ru>
2019-08-28 19:36:36 +0300

    T,Y

--- tmp.ts
+++ tmp.ts
@@ -5,3 +5,11 @@ const last = <T>(arr: T[]) => {
 const l = last([1, 2, 3]);
 
 const l2 = last(['a', 'b', 'c']);
+
+const makeArr = <T, Y>(x: T, y: Y) => {
+  return [x];
+}
+
+const m = makeArr(5, 6);
+
+const m2 = makeArr('a', 'b');
